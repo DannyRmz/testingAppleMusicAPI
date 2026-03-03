d6057017a384e17491421b21c535eb8a31b32d76
Daniel Ramirez <daniel.ramirezm@ingenieria.unam.edu>
2026-03-02 23:25:09 -0600

    first commit

---
+++ testingAMAPI/testingAMAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingAMAPIApp.swift
+//  testingAMAPI
+//
+//  Created by Daniel Ramirez on 02/03/26.
+//
+
+import SwiftUI
+
+@main
+struct testingAMAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
